af24d19a5d64b049346ff9498f2e5d98aa6a40d8
emreyagan1 <ygn.emre02@gmail.com>
2026-09-18 18:43:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function bilgisayarinHazirMi() {
     vscode_version: '1.124.2',
 
     // Node versiyon numarası: örn. "node_version: 'v16.13.0'"
-    node_version: '',
+    node_version: 'v24.11.1',
 
     // Demo projesini forlayıp, clonelayıp çözdükten sonra commit atıp pushladıktan sonra repo url'ini girmek.
-    demo_project_url: '',
+    demo_project_url: 'https://github.com/emreyagan1/FSWEB-S2D4-New-Job',
   };
 
   return checklist;
